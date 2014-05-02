3cfee4839fb46958f7f0a6ac2f843674b994efc5
lectureixdl <lecture@ixdl.kr>
2014-05-02 12:22:28 +0900

    Add device motion

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	console.log("ready");	
+
+	function handleMotionEvent(event) {
+
+	    var x = event.accelerationIncludingGravity.x;
+	    var y = event.accelerationIncludingGravity.y;
+	    var z = event.accelerationIncludingGravity.z;
+
+		$("#xVal").html(x);
+		$("#yVal").html(y);
+		$("#zVal").html(z);
+	}
+
+	window.addEventListener("devicemotion", handleMotionEvent, true);
+
+});
